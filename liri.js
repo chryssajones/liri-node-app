3da65a0ba9a05f27b9d2432a7c944b27ff5814c5
Chryssa <chryssajones@gmail.com>
2017-10-15 00:33:08 -0700

    more code

--- liri.js
+++ liri.js
@@ -1,4 +1,6 @@
 var inquirer = require("inquirer");
+var Twitter = require('twitter');
+
 console.log("Ready!")
 
 function getAction() {
@@ -8,40 +10,57 @@ function getAction() {
 	      type: "input",
 	      message: "What do you want to do?  You can choose my-tweets, spotify-this-song, movie-this, or feeling-lucky.",
 	      name: "chooseAction"
-	    },
-	    {
-	      type: "input",
-	      message: "Which hashtag or song do you want?",
-	      name: "chooseData"
-	    },	    
+	    },    
 	    ])
 	 .then(function(inquirerResponse) {
 	 	var input = (inquirerResponse.chooseAction);
-	 	var data = (inquirerResponse.chooseData);
-	 	console.log(input, data);
-
+	 	console.log("--> Running the " + input + " function");
+		if (input == "my-tweets") {
+			tweets();
+		} else if (input == "spotify-this-song") {
+			inquirer
+	  			.prompt([
+	    			{
+					type: "input",
+					message: "Which song?",
+					name: "chooseSong"
+				    },    
+		    		]).then(function(inquirerResponse) {
+	 					var songData = (inquirerResponse.chooseSong);
+	 					spotify(songData);
+		    		});
+		} else if (input == "movie-this") {
+			inquirer
+	  			.prompt([
+	    			{
+					type: "input",
+					message: "Which movie?",
+					name: "chooseMovie"
+				    },    
+		    		]).then(function(inquirerResponse) {
+	 					var movieData = (inquirerResponse.chooseMovie);
+						movie(movieData);
+		    		});			
+		} else if (input == "feeling-lucky") {
+			doThis();
+		}
 	 });
 };
-
-var input = process.argv[2];
-var data = process.argv[3];
-
-if (input == "my-tweets") {
-	tweets();
-} else if (input == "spotify-this-song") {
-	spotify(data);
-} else if (input == "movie-this") {
-	movie(data);
-} else if (input == "do-what-it-says") {
-	doThis();
-}
 
 
 function tweets () {
 	// show last 20 tweets and when they were tweeted
+	console.log("----------------\nHere are your last 20 tweets:\n-------------------");
+	// var params = {screen_name: 'nodejs'};
+	// client.get('statuses/user_timeline', params, function(error, tweets, response) {
+ //  		if (!error) {
+ //    	console.log(tweets);
+ //  		}
+	// });
 };
 
 function spotify (title) {
+	console.log("Here is the information about the song you requested: " + title);
 	// display:
 	// artist
 	// song name
@@ -51,6 +70,7 @@ function spotify (title) {
 };
 
 function movie(title) {
+	console.log("Here is the information about the movie you requested: " + title);
 	// display:
 	//    * Title of the movie.
  //       * Year the movie came out.
@@ -64,5 +84,8 @@ function movie(title) {
 };
 
 function doThis() {
+	console.log("Surprise! This doesn't do anything.  Hahahahahaha");
 	// code here
 };
+
+getAction();
